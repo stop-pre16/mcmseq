ed04dd11475e6c4d4420c5645f78b1522abad548
Brian Vestal <brian.vestal@ucdenver.edu>
2017-11-16 16:32:26 -0700

    changing to -100

--- src/mcmseq_cpp_functions.cpp
+++ src/mcmseq_cpp_functions.cpp
@@ -40,7 +40,7 @@ double dmvnrm_1f(const arma::vec &x,
   rootisum = arma::sum(log(rooti.diag()));
   z = rooti * arma::trans(x.t() - mean.t()) ;
   tmp = constants - 0.5 * arma::sum(z%z) + rootisum;
-  out_win = std::max(-30.0, tmp);
+  out_win = std::max(-100.0, tmp);
   out = exp(out_win);
   return(out);
 }
